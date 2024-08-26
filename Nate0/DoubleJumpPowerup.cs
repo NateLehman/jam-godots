d46a8862e102531559e9dda652a2b9bbb988dbe7
Nate Lehman <njlehman1@gmail.com>
2024-08-26 19:23:56 -0400

    remove double call of OnBodyEntered

--- Nate0/DoubleJumpPowerup.cs
+++ Nate0/DoubleJumpPowerup.cs
@@ -3,11 +3,6 @@ using System;
 
 public partial class DoubleJumpPowerup : Area2D
 {
-	public override void _Ready()
-	{
-		BodyEntered += OnBodyEntered;
-	}
-
 	private void OnBodyEntered(Node2D body)
 	{
 		if (body is Character character)
